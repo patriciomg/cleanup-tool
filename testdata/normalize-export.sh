f315fbc6f4b9717c8bb17b1f1736bfdc2bb7c85a
Pato <patricio.moya@krankikom.de>
2026-07-30 13:17:27 +0200

    ci: use awk for column-aware mode normalization in export snapshots

--- testdata/normalize-export.sh
+++ testdata/normalize-export.sh
@@ -12,19 +12,27 @@ case "$format" in
     sed -E \
       -e 's|/tmp/cleanup-export-sample-fmt|<SAMPLE_DIR>|g' \
       -e 's/[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?(Z|[+-][0-9]{2}:[0-9]{2})?/<TIMESTAMP>/g' \
-      -e 's/"mode": 2147484141/"mode": <DIR_MODE>/g' \
-      -e 's/"mode": 420/"mode": <FILE_MODE>/g' \
-      -e 's/mode: 2147484141/mode: <DIR_MODE>/g' \
-      -e 's/mode: 420/mode: <FILE_MODE>/g' \
+      -e 's/"mode": [0-9]+/"mode": <MODE>/g' \
+      -e 's/mode: [0-9]+/mode: <MODE>/g' \
       "$file"
     ;;
-  csv|tsv)
+  csv)
+    tmp=$(mktemp)
     sed -E \
       -e 's|/tmp/cleanup-export-sample-fmt|<SAMPLE_DIR>|g' \
       -e 's/[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}/<TIMESTAMP>/g' \
-      -e 's|drwxr-xr-x|<DIR_MODE>|g' \
-      -e 's|-rw-r--r--|<FILE_MODE>|g' \
-      "$file"
+      "$file" > "$tmp"
+    awk 'BEGIN{FS=OFS=","} NR==1{print; next} {if ($7 ~ /^d/){$7="<DIR_MODE>"} else if ($7 ~ /^-/){$7="<FILE_MODE>"}; print}' "$tmp"
+    rm -f "$tmp"
+    ;;
+  tsv)
+    tmp=$(mktemp)
+    sed -E \
+      -e 's|/tmp/cleanup-export-sample-fmt|<SAMPLE_DIR>|g' \
+      -e 's/[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}/<TIMESTAMP>/g' \
+      "$file" > "$tmp"
+    awk 'BEGIN{FS=OFS="\t"} NR==1{print; next} {if ($7 ~ /^d/){$7="<DIR_MODE>"} else if ($7 ~ /^-/){$7="<FILE_MODE>"}; print}' "$tmp"
+    rm -f "$tmp"
     ;;
   *)
     echo "unknown format: $format" >&2
